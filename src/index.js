fae59dcf08a790319b810feddc5845c4e4c6538a
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-08-31 01:29:22 +0200

    feat: implemented brackets func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let conf = bracketsConfig.map(el => el.join(''))
+	for (let i = 0; i < conf.length; i++) {
+		if (str.includes(conf[i])) {
+			str = str.replace(conf[i], '')
+			i = -1
+		}
+	}
+	return str.length === 0
 }
